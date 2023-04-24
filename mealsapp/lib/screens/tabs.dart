08dac9bd9b5d5702797eebae8f93d4d08ec67f04
HMEI83 <101045819+HMEI83@users.noreply.github.com>
2023-04-24 12:39:36 +1000

    Adding a side drawer

--- mealsapp/lib/screens/tabs.dart
+++ mealsapp/lib/screens/tabs.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mealsapp/screens/categories.dart';
 import 'package:mealsapp/screens/meals.dart';
+import 'package:mealsapp/widgets/main_drawer.dart';
 
 import '../models/meal.dart';
 
@@ -64,6 +65,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(activePageTitle),
       ),
+      drawer: const MainDrawer(),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectedPage,
